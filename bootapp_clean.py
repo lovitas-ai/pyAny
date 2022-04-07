54182cadb16de4c503a62da1aeb0165d821231ac
lovitas-ai <lovitas@hotmail.de>
2022-04-07 09:12:49 +0200

    adding map

--- bootapp_clean.py
+++ bootapp_clean.py
@@ -8,11 +8,14 @@ import plotly.graph_objects as gp
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import geopandas
 
 #Data
-
+kreis_map=geopandas.read_file('landkreise_s.geojson') 
 df_01=pd.read_csv('pyramid.csv')
 df_02=pd.read_csv('infobox.csv')
+df_02['AGS'] = df_02['AGS'].astype(str)
+df_02['AGS'] = df_02['AGS'].str.zfill(5)
 app= dash.Dash((__name__),external_stylesheets=[dbc.themes.FLATLY],
             meta_tags=[{'name': 'viewport',
                            'content': 'width=device-width, initial-scale=1.0'}]
@@ -31,7 +34,7 @@ app.layout = dbc.Container([
                 [html.P("Wählen Sie den Kreis",className='text-left text-secundary mb-4'), 
             dcc.Dropdown(id='kreis', options=[{'label': x,'value': x} for x in sorted(df_01['Kreis'].unique())], value='Ansbach (Lkr)',className='dropdown'),
             html.P("Wählen Sie das Jahr",className='text-left text-secundary mb-4 pt-3 '), 
-            dcc.Slider(id='year', min = 2000, max=2020,step =1, value=2020,className='slider', marks={
+            dcc.Slider(id='year', min = 2000, max=2020,step =1, value=2020,className='slider',updatemode='drag', marks={
                     2000: '2000',2001:'2001',2002:'2002',2003:'2003',2004:'2004',2005:'2005',2006:'2006',2007:'2007',2008:'2008',2009:'2009',2010:'2010',
                     2011:'2011',2012:'2012',2013:'2013',2014:'2014',2015:'2015',2016:'2016',2017:'2017',2018:'2018',2019:'2019',2020:'2020',})]
                     ,width={'size':9,'offset':1,'order':1}),
@@ -57,8 +60,21 @@ app.layout = dbc.Container([
                 html.P(" ",className='text-left text-primary mb-4'),
                 dcc.Graph(id='einw_graph',figure={},className='divBorder')
             ], width={'size':9,'offset':1,'order':1})
-            ], justify = 'start')
-        ], fluid=True, className='body' )
+            ], justify = 'start'),
+        dbc.Row([
+            dbc.Col([ 
+                    html.P(" ",className='text-left text-primary mb-4'),
+                    html.H2("Kartenanalysen",className='text-left text-primary mb-4'),
+                    dcc.Slider(id='year_map', min = 2000, max=2020,step =1, value=2020,className='slider',updatemode='drag', marks={
+                    2000: '2000',2001:'2001',2002:'2002',2003:'2003',2004:'2004',2005:'2005',2006:'2006',2007:'2007',2008:'2008',2009:'2009',2010:'2010',
+                    2011:'2011',2012:'2012',2013:'2013',2014:'2014',2015:'2015',2016:'2016',2017:'2017',2018:'2018',2019:'2019',2020:'2020',}),
+                    dcc.RadioItems(id='radio_map',options=[
+                    {'label': 'Durchschnittsalter', 'value': 'D_Alter'},
+                    {'label': 'Geschlechterverhältnis', 'value': 'Geschl_v'}],value='D_Alter',inline=True, className='mb-4'),
+                    dcc.Graph(id='graph_map',figure={},className='divBorder'),
+            ], width={'size':9,'offset':1,'order':1})
+        ],justify = 'start')
+         ], fluid=True, className='body' )
 
 
 @app.callback(
@@ -173,6 +189,20 @@ def update_my_graph2(kreis_val):
     ) )              
     return fig
 
+
+@app.callback(
+    Output(component_id='graph_map', component_property='figure'),
+    [Input(component_id='year_map', component_property='value')],
+    [Input(component_id='radio_map', component_property='value')],
+    prevent_initial_call=False
+    )
+def update_my_graph2(year_map,radio_map):
+    dff_02_01 = df_02[(df_02["J"]==(year_map))]
+    fig = px.choropleth(dff_02_01,featureidkey='properties.AGS', geojson=kreis_map, locations='AGS', color=radio_map,
+                    projection="mercator",color_continuous_scale="Viridis", hover_name='Kreis')
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(title_text = 'Bayern',margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
 
 
 if __name__=='__main__':
